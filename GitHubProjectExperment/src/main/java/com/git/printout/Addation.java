ea71c99c5e983d432bae4e640d4055051fad4a33
kiran <reddemkiran138@gmail.com>
2020-12-19 17:44:04 +0530

    Start coading

--- GitHubProjectExperment/src/main/java/com/git/printout/Addation.java
+++ GitHubProjectExperment/src/main/java/com/git/printout/Addation.java
@@ -2,6 +2,7 @@ package com.git.printout;
 
 public class Addation {
 	public static void main(String[] args) {
-		System.out.println("Welcome to git expermenyt");
+		//System.out.println("Welcome to git expermenyt");
+		System.out.println("you can experment on your your tasks");
 	}
 }
